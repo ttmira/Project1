fed937bc0a18aa038c20dfa86aac657ed7ffb4e4
Tamara Kazybaeva <tamara.kazybaeva2005@gmail.com>
2024-09-13 16:10:27 +0500

    first commit

---
+++ src/test/java/com/example/projects1/CheckHTTPResponse.java
@@ -0,0 +1,26 @@
+package com.example.projects1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+public class CheckHTTPResponse {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public  void shouldPassIfStringMatches(){
+        assertEquals("Hello World from Tamara IT2-2215",
+                testRestTemplate.getForObject("http://localhost:"+port+"/",
+                String.class));
+    }
+}
